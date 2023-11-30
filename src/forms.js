ada2a797c476ac2251a672605af6a13bf14151cd
Nicholas Gulachek <gulac004@umn.edu>
2023-11-29 23:49:20 -0600

    watch form submission

--- src/forms.js
+++ src/forms.js
@@ -1,1 +1,18 @@
-alert('hello forms.js!');
+window.addEventListener('submit', (e) => {
+	const form = e.target;
+	if (!form) return;
+	if (form.hasAttribute('action')) return;
+
+	const method = form.getAttribute('method');
+	if (method && method !== 'POST') return;
+
+	// Form w/o action and default (POST) form. Submit data
+	e.preventDefault(); // don't actually submit
+	const data = new FormData(form);
+	const params = new URLSearchParams(data);
+
+	const xhr = new XMLHttpRequest();
+	xhr.open('POST', '~/submit');
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send(params.toString());
+});
