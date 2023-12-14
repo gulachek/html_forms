11246d1d180fca309f0f17cfee7c885755c6ac21
Nicholas Gulachek <gulac004@umn.edu>
2023-12-14 13:37:29 -0600

    make a rule class for webpack

--- make.mjs
+++ make.mjs
@@ -5,6 +5,82 @@ import { platform } from 'node:os';
 import webpack from 'webpack';
 import { dirname, basename } from 'node:path';
 
+class WebpackRule {
+	wp;
+	config;
+	srcPath;
+	outPath;
+
+	constructor(opts, srcPath, outPath, config) {
+		if (opts.isDevelopment) {
+			config.mode = 'development';
+			config.devtool = 'inline-source-map';
+		}
+
+		this.config = config;
+		this.srcPath = Path.src(srcPath);
+		this.outPath = Path.build(outPath);
+	}
+
+	prereqs() {
+		return [this.srcPath];
+	}
+
+	targets() {
+		return [this.outPath];
+	}
+
+	recipe(args) {
+		const [src, out] = args.absAll(this.srcPath, this.outPath);
+
+		if (!this.wp) {
+			const config = this.config;
+			config.entry = src;
+			config.output = config.output || {};
+			config.output.path = dirname(out);
+			config.output.filename = basename(out);
+			config.module = {
+				rules: [
+					{
+						test: /\.ts$/,
+						use: 'ts-loader',
+						exclude: /node_modules/,
+					},
+				],
+			};
+
+			config.resolve = {
+				extensions: ['.ts', '.js'],
+			};
+			this.wp = webpack(this.config);
+		}
+
+		return new Promise((res) => {
+			this.wp.run((err, stats) => {
+				const statsJson = stats.toJson();
+				statsJson.modules.forEach((mod) => {
+					if (mod.nameForCondition) {
+						args.addPostreq(mod.nameForCondition);
+					}
+				});
+
+				err && args.logStream.write(err);
+				args.logStream.write(stats.toString({ colors: true }));
+				res(!(err || stats.hasErrors()));
+
+				this.wp.close((closeErr) => {
+					closeErr && console.error('Error closing webpack', closeErr);
+				});
+			});
+		});
+	}
+}
+
+function addWebpack(book, opts, srcPath, outPath, config) {
+	const wp = new WebpackRule(opts, srcPath, outPath, config);
+	book.add(wp);
+}
+
 cli((book, opts) => {
 	const c = new C(platformCompiler(), {
 		...opts,
@@ -47,8 +123,9 @@ cli((book, opts) => {
 	const wpDir = Path.build('webpack');
 	const formsJsBundle = wpDir.join('forms.js');
 	const browserBundle = wpDir.join('browser.cjs');
+
 	addWebpack(book, opts, formsTs, formsJsBundle, {
-		target: 'web',
+		target: 'electron-main',
 		output: {
 			library: {
 				type: 'var',
@@ -126,60 +203,6 @@ cli((book, opts) => {
 		);
 	});
 });
-
-function addWebpack(book, opts, srcPath, outPath, baseConfig) {
-	book.add(outPath, [srcPath], (args) => {
-		const [src, out] = args.absAll(srcPath, outPath);
-
-		const config = {
-			...baseConfig,
-			entry: src,
-			output: {
-				path: dirname(out),
-				filename: basename(out),
-				...baseConfig.output,
-			},
-			module: {
-				rules: [
-					{
-						test: /\.ts$/,
-						use: 'ts-loader',
-						exclude: /node_modules/,
-					},
-				],
-			},
-			resolve: {
-				extensions: ['.ts', '.js'],
-			},
-		};
-
-		if (opts.isDevelopment) {
-			config.mode = 'development';
-			config.devtool = 'inline-source-map';
-		}
-
-		const wp = webpack(config);
-
-		return new Promise((res) => {
-			wp.run((err, stats) => {
-				const statsJson = stats.toJson();
-				statsJson.modules.forEach((mod) => {
-					if (mod.nameForCondition) {
-						args.addPostreq(mod.nameForCondition);
-					}
-				});
-
-				err && args.logStream.write(err);
-				args.logStream.write(stats.toString({ colors: true }));
-				res(!(err || stats.hasErrors()));
-
-				wp.close((closeErr) => {
-					console.error('Error closing webpack', closeErr);
-				});
-			});
-		});
-	});
-}
 
 function bufToCppArray(identifier, buf) {
 	const array = `${identifier}_array__`;
