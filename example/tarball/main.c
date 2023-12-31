25f265aeec29c733f432c0fcec87cd1771c09aff
Nicholas Gulachek <gulac004@umn.edu>
2023-12-30 21:47:00 -0600

    add error messages to tarball example

--- example/tarball/main.c
+++ example/tarball/main.c
@@ -7,16 +7,20 @@ int main() {
   html_connection *con;
 
   if (!html_connect(&con)) {
-    // TODO - print error in connection
-    fprintf(stderr, "Failed to make html connection\n");
+    fprintf(stderr, "Failed to make html connection: %s\n", html_errmsg(con));
     return 1;
   }
 
-  if (!html_upload_archive(con, "/", TARBALL_PATH))
+  if (!html_upload_archive(con, "/", TARBALL_PATH)) {
+    fprintf(stderr, "Failed to upload docroot archive: %s\n", html_errmsg(con));
     return 1;
+  }
 
-  if (!html_navigate(con, "/index.html"))
+  if (!html_navigate(con, "/index.html")) {
+    fprintf(stderr, "Failed to navigate to /index.html: %s\n",
+            html_errmsg(con));
     return 1;
+  }
 
   html_form form = NULL;
   html_read_form(con, &form);
