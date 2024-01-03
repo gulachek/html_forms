6072a8ef7634f7c0a238193b57460d1a46ef1da4
Nicholas Gulachek <gulac004@umn.edu>
2024-01-03 16:29:47 -0600

    Stop using XHR for form submission

--- src/forms.ts
+++ src/forms.ts
@@ -1,20 +1,13 @@
 window.addEventListener('submit', (e) => {
 	const form = e.target as HTMLFormElement;
 	if (!form) return;
-	if (form.hasAttribute('action')) return;
+	if (!form.hasAttribute('action')) {
+		form.action = '~/submit';
+	}
 
-	const method = form.getAttribute('method');
-	if (method && method !== 'POST') return;
-
-	// Form w/o action and default (POST) form. Submit data
-	e.preventDefault(); // don't actually submit
-	const data = new FormData(form, e.submitter);
-	const params = new URLSearchParams(data as any);
-
-	const xhr = new XMLHttpRequest();
-	xhr.open('POST', '~/submit');
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.send(params.toString());
+	if (!form.hasAttribute('method')) {
+		form.method = 'POST';
+	}
 });
 
 export function connect(): WebSocket {
